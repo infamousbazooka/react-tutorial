43f5bb72e89d0bcd31a366d802a8eab58dcf8371
infamousbazooka <magnusfernandes1295@gmail.com>
2017-08-25 23:23:35 +0530

    Components

--- src/components/SkiDayCount.js
+++ src/components/SkiDayCount.js
@@ -6,13 +6,19 @@ export const SkiDayCount = React.createClass({
     return (
       <div className="ski-day-count">
         <div className="total-days">
+          <span>{this.props.total}</span>
           <span>5 days</span>
         </div>
         <div className="powder-days">
+          <span>{this.props.powder}</span>
           <span>2 days</span>
         </div>
         <div className="backcountry-days">
+          <span>{this.props.backcountry}</span>
           <span>1 hiking days</span>
+        </div>
+        <div>
+          <span>{this.props.goal}</span>
         </div>
       </div>
     )
